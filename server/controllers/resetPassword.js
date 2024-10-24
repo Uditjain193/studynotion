8fab1faa2da592debb1cf9015dccb2d604e00802
Udit Jain <uditjain198@example.com>
2024-10-24 21:52:51 +0530

    changes

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
     console.log("DETAILS", updatedDetails)
 
     // const url = `http://localhost:3000/update-password/${token}`
-    const url = `https://codeplay-edtech-project.vercel.app/update-password/${token}`
+    const url = `https://studynotion-chi.vercel.app/update-password/${token}`
 
     await mailSender(
       email,
